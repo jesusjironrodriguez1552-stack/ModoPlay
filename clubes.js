3c3d80d677bd3dd8a84e97de40673aa5a6a92327
alexander99 <jesusjironrodriguez155.2@gmail.com>
2025-08-29 11:26:08 -0500

    Update clubes.js

--- clubes.js
+++ clubes.js
@@ -1,18 +1,27 @@
 // EQUIPOS PARTICIPANTES EN LA LIGA EFOOTBALL 2025
 // Para agregar o quitar equipos, simplemente modifica este array
-const clubes = [
-    { name: "Royyer10", club: "Inter" },
-    { name: "Benny Caballero", club: "FC Barcelona" },
-    { name: "LW🐉", club: "AC Milan" },
-    { name: "the Andres", club: "Real Madrid" },
-    { name: "alexander", club: "Santos" },
-    { name: "399633", club: "Newcastle" },
-    { name: "ElianVz17", club: "Venezuela" },
-    { name: "Brn_14", club: "Arsenal" },
-    { name: "Ever Gutierrez", club: "SC Corinthians" },
-    { name: "Santislowly", club: "Club América" },
-    { name: "Máx", club: "Botafogo" }
+const jugadores = [
+  { name: "Royyer10", club: "Inter" },
+  { name: "Benny Caballero", club: "FC Barcelona" },
+  { name: "LW🐉", club: "AC Milan" },
+  { name: "the Andres", club: "Real Madrid" },
+  { name: "alexander", club: "Santos" },
+  { name: "399633", club: "Newcastle" },
+  { name: "ElianVz17", club: "Venezuela" },
+  { name: "Brn_14", club: "Arsenal", id: "ASEZ-609-571-075" },
+  { name: "Ever Gutierrez", club: "SC Corinthians" },
+  { name: "Santislowly", club: "Club América" },
+  { name: "Máx", club: "Botafogo" },
+  { name: "Bangbrousnetwork", club: "Ajax", id: "ASQL-020-982-058" },
+  { name: "Angel3.1416", club: "Club América", id: "ASAA-031-059-500" },
+  { name: "DorianBU", club: "FC Barcelona", id: "ASAA-064-323-667" },
+  { name: "Alberto Sosa", club: "Manchester United", id: "ASAA-738-732-120" },
+  { name: "carlosandreess", club: "Junior FC", id: "ASAA-000-436-279" },
+  { name: "Esteban David Meriño", club: "Atlético Nacional", id: "ASNQ-494-301-322" },
+  { name: "BRANZZINO-777", club: "Japón", id: "ASDN-004-617-039" },
+  { name: "Carlos Andrés Hurtado Cuero", club: "Gomito58", id: "ASAA-789-208-113" }
 ];
+
 // ... tu código existente ...
 
 // Exportación por defecto
